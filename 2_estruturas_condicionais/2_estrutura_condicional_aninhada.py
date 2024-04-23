c655a9407a9a8faa0c13adfe15303b6e51140e96
Dalila Custódio <dalila.dalila70@gmail.com>
2024-04-23 11:22:03 -0300

    code: 2_estrutura_condicional_aninhada

---
+++ 2_estruturas_condicionais/2_estrutura_condicional_aninhada.py
@@ -0,0 +1,25 @@
+# Estrutura Condicional Aninhada.
+
+conta_normal = False
+conta_universitaria = False
+
+saldo = 2000
+saque = 1500
+cheque_especial = 450
+
+if conta_normal:
+    if saldo >= saque:
+        print("Saque realizado com sucesso!")
+    elif saque <= (saldo + cheque_especial):
+        print("Saque realizado com uso do chaque especial!")
+    else:
+        print("Não foi possivel realizar o saque, saldo insuficiente!")
+
+elif conta_universitaria:
+    if saldo >= saque:
+        print("Saque realizado com sucesso!")
+    else:
+        print("Saldo insuficiente!")
+
+else:
+    print("Sistema não reconheceu seu tipo de conta, entre em contato com seu gerente.")            
