fac7e7aa9b33ddfac68aa77770f50060c4b8b34a
Dalila Custódio <dalila.dalila70@gmail.com>
2024-04-23 11:23:36 -0300

    code: 3_estrutura_condicional_ternaria

---
+++ 2_estruturas_condicionais/3_estrutura_condicional_ternaria.py
@@ -0,0 +1,9 @@
+# Estrutura Condicional Ternária.
+
+saldo = 500
+saque = 200
+
+status = "Sucesso" if saldo >= saque else "Falha"
+
+print(f"{status} ao realizar o saque!")
+# Result: Sucesso ao realizar o saque!.
