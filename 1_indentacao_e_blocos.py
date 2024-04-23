cb075a20ce613b678d1e4273daec4528a49d617d
Dalila Custódio <dalila.dalila70@gmail.com>
2024-04-23 11:18:11 -0300

    code: 1_indentacao_e_blocos

---
+++ 1_indentacao_e_blocos.py
@@ -0,0 +1,18 @@
+# Python só deixa exercultar um código se tiver Indentação.
+
+def sacar(valor):
+    saldo = 500
+
+    if saldo >= valor:
+        print("valor sacado!")
+        print("retire o seu dinheiro na boca do caixa.")
+
+    print("Obrigado por ser nosso cliente, tenha um bom dia!")
+
+def depositar(valor):
+    saldo = 500
+    saldo += valor
+
+sacar(10000)
+
+# Result: Obrigado por ser nosso cliente, tenha um bom dia!
